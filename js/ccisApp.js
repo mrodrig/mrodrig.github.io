6943d4942a46a7fc8b66ecb15f1dea07ee5967f7
mrodrig <rodrigues.mi@husky.neu.edu>
2015-06-12 11:26:58 -0400

    Working fix for ngRoutes on Apache

--- js/ccisApp.js
+++ js/ccisApp.js
@@ -15,18 +15,17 @@
             $httpProvider.defaults.headers.get = {};
         }
 
-        //TODO: See if we can remove this
         //disable IE ajax request caching
         $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
 
         //The routes that our angular app will handle
         $routeProvider
             .when('/', {templateUrl: 'pages/home/homePage.html', controller: 'HomePageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
-            .when('/home', {templateUrl: 'pages/home/homePage.html', controller: 'HomePageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
-            .when('/resume', {templateUrl: 'pages/resume/resumePage.html', controller: 'ResumePageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
-            .when('/projects', {templateUrl: 'pages/projects/projectsPage.html', controller: 'ProjectsPageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
-            .when('/interests', {templateUrl: 'pages/interests/interestsPage.html', controller: 'InterestsPageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
-            .when('/contact', {templateUrl: 'pages/contact/contactPage.html', controller: 'ContactPageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
+            .when('/index.html', {templateUrl: 'pages/home/homePage.html', controller: 'HomePageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true})
+            .when('/resume.html', {templateUrl: 'pages/resume/resumePage.html', controller: 'ResumePageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
+            .when('/projects.html', {templateUrl: 'pages/projects/projectsPage.html', controller: 'ProjectsPageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
+            .when('/interests.html', {templateUrl: 'pages/interests/interestsPage.html', controller: 'InterestsPageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
+            .when('/contact.html', {templateUrl: 'pages/contact/contactPage.html', controller: 'ContactPageController', controllerAs: 'pageCtrl', caseInsensitiveMatch: true })
             .when('/notfound', { templateUrl: 'partials/404.html'})
             .otherwise({redirectTo: '/notfound'});
     }
