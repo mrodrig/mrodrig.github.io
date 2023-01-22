dc7de90602dd9f741c2c87b52effa061d9708e6f
mrodrig <rodrigues.mi@northeastern.edu>
2023-01-22 06:12:41 +0000

    refactor: break out projects components

---
+++ src/components/ProjectsList.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="projects">
+        <LoadingSpinner :loading="loading" />
+
+        <div id="server" class="projects-group">
+            <h2>Website/Server Development and Management</h2>
+            <project v-for="serverProject in serverProjects" :key="serverProject.name" :project="serverProject" :type="'server'" />
+        </div>
+
+        <div id="courses" class="projects-group">
+            <h2>Course Projects</h2>
+            <project v-for="courseProject in courseProjects" :key="courseProject.name" :project="courseProject" :type="'course'" />
+        </div>
+
+        <div id="npm" class="projects-group">
+            <h2>Node Modules</h2>
+            <project v-for="npmProject in npmProjects" :key="npmProject.name" :project="npmProject" :type="'npm'" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+import Project from '@/components/Project.vue';
+import Firebase from '@/services/firebase';
+import Logging from '@/services/logging';
+import { FirestoreCollection } from '@/models/firestoreCollections';
+import projects from '@/data/projects';
+
+export default defineComponent({
+    name: 'ProjectsView',
+    components: {
+        Project,
+        LoadingSpinner,
+    },
+    data () {
+        return {
+            loading: true,
+            projects: [],
+            error: false,
+            npmProjects: projects.npmProjects,
+            serverProjects: projects.serverProjects,
+            courseProjects: projects.courseProjects,
+        };
+    },
+    async created () {
+        try {
+            const docs = await Firebase.getDocsFromCollection(FirestoreCollection.Projects);
+            this.loading = false;
+            // this.projects = docs;
+            console.log('Documents', JSON.stringify(docs, null, 4));
+        } catch (error) {
+            this.error = true;
+            Logging.error('Error getting projects', error);
+        }
+    },
+});
+</script>
+
+<style lang="less">
+    @import '@/less/constants.less';
+
+    #projects {
+        margin: 0 2em;
+
+        .projects-group {
+            padding-top: 1em;
+        }
+    }
+
+    @media @upToNarrowQuery {
+
+    }
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,53 +1,17 @@
 <template>
-    <div id="projects">
-        <div id="server" class="projects-group">
-            <h2>Website/Server Development and Management</h2>
-            <project v-for="serverProject in serverProjects" :key="serverProject.name" :project="serverProject" :type="'server'" />
-        </div>
-
-        <div id="courses" class="projects-group">
-            <h2>Course Projects</h2>
-            <project v-for="courseProject in courseProjects" :key="courseProject.name" :project="courseProject" :type="'course'" />
-        </div>
-
-        <div id="npm" class="projects-group">
-            <h2>Node Modules</h2>
-            <project v-for="npmProject in npmProjects" :key="npmProject.name" :project="npmProject" :type="'npm'" />
-        </div>
-    </div>
+    <ProjectsList />
 </template>
 
 <script lang="ts">
-import project from '@/components/Project.vue';
-// import Firebase from '@/services/firebase';
-// import { FirestoreCollection } from '@/models/firestoreCollections';
-import projects from '@/data/projects';
+import { defineComponent } from 'vue';
+import ProjectsList from '@/components/ProjectsList.vue';
 
-export default {
+export default defineComponent({
     name: 'ProjectsView',
     components: {
-        project,
+        ProjectsList,
     },
-    data () {
-        return {
-            projects: [],
-            error: false,
-            npmProjects: projects.npmProjects,
-            serverProjects: projects.serverProjects,
-            courseProjects: projects.courseProjects,
-        };
-    },
-    async created () {
-        try {
-            // const docs = await Firebase.getDocsFromCollection(FirestoreCollection.Projects);
-            // this.projects = docs;
-            // console.log('Documents', JSON.stringify(docs, null, 4));
-        } catch (error) {
-            // this.error = true;
-            console.error('Error getting projects', error);
-        }
-    },
-};
+});
 </script>
 
 <style lang="less">
